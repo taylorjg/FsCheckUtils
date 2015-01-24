00eca995a9a1d8048a0e71243a6cb69ac37517d8
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-24 22:41:15 +0000

    Reordered methods

--- FsCheckUtils/PropExtensions.cs
+++ FsCheckUtils/PropExtensions.cs
@@ -10,14 +10,14 @@ namespace FsCheckUtils
             return PropOperators.op_DotAmpDot(l, r);
         }
 
-        public static Gen<Rose<Result>> Or<TLeftTestable, TRightTestable>(TLeftTestable l, TRightTestable r)
-        {
-            return PropOperators.op_DotBarDot(l, r);
-        }
-
         public static Gen<Rose<Result>> AndAll<TTestable>(params TTestable[] assertions)
         {
             return assertions.Aggregate(Prop.ofTestable(true), And);
+        }
+
+        public static Gen<Rose<Result>> Or<TLeftTestable, TRightTestable>(TLeftTestable l, TRightTestable r)
+        {
+            return PropOperators.op_DotBarDot(l, r);
         }
 
         public static Gen<Rose<Result>> OrAll<TTestable>(params TTestable[] assertions)
