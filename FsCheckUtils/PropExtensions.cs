edc1ae0cd4e90270d9ddd9370ee56619e07e7a0d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-27 19:34:02 +0000

    Added some xml doc comments and unit tests re Prop operator wrappers

--- FsCheckUtils/PropExtensions.cs
+++ FsCheckUtils/PropExtensions.cs
@@ -5,31 +5,73 @@ namespace FsCheckUtils
 {
     public static class PropExtensions
     {
+        /// <summary>
+        /// Wrapper method around FsCheck's .&. operator that succeeds if both properties succeed.
+        /// </summary>
+        /// <typeparam name="TLeftTestable">Type of the left property.</typeparam>
+        /// <typeparam name="TRightTestable">Type of the right property.</typeparam>
+        /// <param name="l">The left property.</param>
+        /// <param name="r">The right property.</param>
+        /// <returns>A property that succeeds if both properties succeed.</returns>
         public static Gen<Rose<Result>> And<TLeftTestable, TRightTestable>(TLeftTestable l, TRightTestable r)
         {
             return PropOperators.op_DotAmpDot(l, r);
         }
 
+        /// <summary>
+        /// Convenience method that applies FsCheck's .&. operator to a collection of properties and succeeds if all properties succeed.
+        /// </summary>
+        /// <typeparam name="TTestable">Type of the properties in the collection. They must all be of the same type.</typeparam>
+        /// <param name="assertions">The collection of properties.</param>
+        /// <returns>A property that succeeds if all properties succeed.</returns>
         public static Gen<Rose<Result>> AndAll<TTestable>(params TTestable[] assertions)
         {
             return assertions.Aggregate(Prop.ofTestable(true), And);
         }
 
+        /// <summary>
+        /// Wrapper method around FsCheck's .|. operator that fails if both properties fail.
+        /// </summary>
+        /// <typeparam name="TLeftTestable">Type of the left property.</typeparam>
+        /// <typeparam name="TRightTestable">Type of the right property.</typeparam>
+        /// <param name="l">The left property.</param>
+        /// <param name="r">The right property.</param>
+        /// <returns>A property that fails if both properties fail.</returns>
         public static Gen<Rose<Result>> Or<TLeftTestable, TRightTestable>(TLeftTestable l, TRightTestable r)
         {
             return PropOperators.op_DotBarDot(l, r);
         }
 
+        /// <summary>
+        /// Convenience method that applies FsCheck's .|. operator to a collection of properties and fails if all properties fail.
+        /// </summary>
+        /// <typeparam name="TTestable">Type of the properties in the collection. They must all be of the same type.</typeparam>
+        /// <param name="assertions">The collection of properties.</param>
+        /// <returns>A property that fails if all properties fail.</returns>
         public static Gen<Rose<Result>> OrAll<TTestable>(params TTestable[] assertions)
         {
             return assertions.Aggregate(Prop.ofTestable(false), Or);
         }
 
+        /// <summary>
+        /// Wrapper method around FsCheck's |@ property operator.
+        /// </summary>
+        /// <typeparam name="TTestable">Type of the property.</typeparam>
+        /// <param name="assertion">The property to be labelled.</param>
+        /// <param name="name">The label to apply to the property.</param>
+        /// <returns>A property</returns>
         public static Gen<Rose<Result>> Label<TTestable>(TTestable assertion, string name)
         {
             return PropOperators.op_BarAt(assertion, name);
         }
 
+        /// <summary>
+        /// Wrapper method around FsCheck's ==> implication operator.
+        /// </summary>
+        /// <typeparam name="TTestable">Type of the property.</typeparam>
+        /// <param name="condition">The condition which, if true, implies that the property should be tested.</param>
+        /// <param name="assertion">The property to test if the condition is true.</param>
+        /// <returns></returns>
         public static Gen<Rose<Result>> Implies<TTestable>(bool condition, TTestable assertion)
         {
             return PropOperators.op_EqualsEqualsGreater(condition, assertion);
--- UnitTests/PropExtensionsTests.cs
+++ UnitTests/PropExtensionsTests.cs
@@ -13,19 +13,13 @@ namespace UnitTests
     [TestFixture]
     public class PropExtensionsTests
     {
-        private static Property AndProperties(int n, Func<int, bool> leftPropertyFunc, Func<int, bool> rightPropertyFunc)
-        {
-            var p1 = PropExtensions.Label(leftPropertyFunc(n), "Left property");
-            var p2 = PropExtensions.Label(rightPropertyFunc(n), "Right property");
-            return PropExtensions.And(p1, p2);
-        }
-
         [Test]
         public void AndSucceedsWhenBothPropertiesSucceed()
         {
             Func<int, bool> leftPropertyFunc = _ => true;
             Func<int, bool> rightPropertyFunc = _ => true;
-            var andPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(n => AndProperties(n, leftPropertyFunc, rightPropertyFunc));
+            var andPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => AndProperties(n, leftPropertyFunc, rightPropertyFunc));
             Check.QuickThrowOnFailure(andPropertiesFsFunc);
         }
 
@@ -36,16 +30,10 @@ namespace UnitTests
         {
             Func<int, bool> leftPropertyFunc = _ => leftResult;
             Func<int, bool> rightPropertyFunc = _ => rightResult;
-            var andPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(n => AndProperties(n, leftPropertyFunc, rightPropertyFunc));
+            var andPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => AndProperties(n, leftPropertyFunc, rightPropertyFunc));
             var ex = Assert.Throws<Exception>(() => Check.QuickThrowOnFailure(andPropertiesFsFunc));
             Assert.That(ex.Message, Is.StringStarting("Falsifiable"));
-        }
-
-        private static Property OrProperties(int n, Func<int, bool> leftPropertyFunc, Func<int, bool> rightPropertyFunc)
-        {
-            var p1 = PropExtensions.Label(leftPropertyFunc(n), "Left property");
-            var p2 = PropExtensions.Label(rightPropertyFunc(n), "Right property");
-            return PropExtensions.Or(p1, p2);
         }
 
         [TestCase(true, true)]
@@ -55,7 +43,8 @@ namespace UnitTests
         {
             Func<int, bool> leftPropertyFunc = _ => leftResult;
             Func<int, bool> rightPropertyFunc = _ => rightResult;
-            var orPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(n => OrProperties(n, leftPropertyFunc, rightPropertyFunc));
+            var orPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => OrProperties(n, leftPropertyFunc, rightPropertyFunc));
             Check.QuickThrowOnFailure(orPropertiesFsFunc);
         }
 
@@ -64,41 +53,88 @@ namespace UnitTests
         {
             Func<int, bool> leftPropertyFunc = _ => false;
             Func<int, bool> rightPropertyFunc = _ => false;
-            var orPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(n => OrProperties(n, leftPropertyFunc, rightPropertyFunc));
+            var orPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => OrProperties(n, leftPropertyFunc, rightPropertyFunc));
             var ex = Assert.Throws<Exception>(() => Check.QuickThrowOnFailure(orPropertiesFsFunc));
             Assert.That(ex.Message, Is.StringStarting("Falsifiable"));
         }
 
-        private static Property AndAllProperties(int n, IEnumerable<Func<int, bool>> propertyFuncs)
-        {
-            var index = 0;
-            var properties = propertyFuncs.Select(pf => PropExtensions.Label(pf(n), string.Format("Property[{0}]", index++)));
-            return PropExtensions.AndAll(properties.ToArray());
-        }
-
-        [TestCase(new bool[]{})]
-        [TestCase(new[] { true})]
-        [TestCase(true, true)]
-        [TestCase(true, true, true)]
-        public void AndAllSucceedsWhenAllPropertiesSucceed(params bool[] results)
+        [TestCase(1, new bool[]{}, TestName = "Params array empty")]
+        [TestCase(2, new []{true}, TestName = "Params array with single true element")]
+        [TestCase(3, true, true)]
+        [TestCase(4, true, true, true)]
+        public void AndAllSucceedsWhenAllPropertiesSucceed(int dummyUnique, params bool[] results)
         {
             var propertyFuncs = results.Select(result => new Func<int, bool>(_ => result));
-            var andAllPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(n => AndAllProperties(n, propertyFuncs));
+            var andAllPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => AndAllProperties(n, propertyFuncs));
             Check.QuickThrowOnFailure(andAllPropertiesFsFunc);
         }
 
-        [TestCase(new[]{false})]
-        [TestCase(false, false)]
-        [TestCase(false, false, false)]
-        [TestCase(true, false)]
-        [TestCase(false, true)]
-        [TestCase(false, false, false, false, false, true)]
-        public void AndAllFailsWhenAnyPropertyFails(params bool[] results)
+        [TestCase(1, new[]{false}, TestName = "Params array with single false element")]
+        [TestCase(2, false, false)]
+        [TestCase(3, false, false, false)]
+        [TestCase(4, true, false)]
+        [TestCase(5, false, true)]
+        [TestCase(6, false, false, false, false, false, true)]
+        public void AndAllFailsWhenAnyPropertyFails(int dummyUnique, params bool[] results)
         {
             var propertyFuncs = results.Select(result => new Func<int, bool>(_ => result));
-            var andAllPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(n => AndAllProperties(n, propertyFuncs));
+            var andAllPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => AndAllProperties(n, propertyFuncs));
             var ex = Assert.Throws<Exception>(() => Check.QuickThrowOnFailure(andAllPropertiesFsFunc));
             Assert.That(ex.Message, Is.StringStarting("Falsifiable"));
+        }
+
+        [TestCase(1, new[]{true}, TestName = "Params array with single true element")]
+        [TestCase(2, false, true)]
+        [TestCase(3, true, false)]
+        [TestCase(4, false, false, false, false, true)]
+        public void OrAllSucceedsWhenAnyPropertySucceeds(int dummyUnique, params bool[] results)
+        {
+            var propertyFuncs = results.Select(result => new Func<int, bool>(_ => result));
+            var orAllPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => OrAllProperties(n, propertyFuncs));
+            Check.QuickThrowOnFailure(orAllPropertiesFsFunc);
+        }
+
+        [TestCase(1, new[] { false }, TestName = "Params array with single false element")]
+        [TestCase(2, false, false)]
+        [TestCase(3, false, false, false)]
+        [TestCase(4, false, false, false, false)]
+        public void OrAllFailsWhenAllPropertiesFail(int dummyUnique, params bool[] results)
+        {
+            var propertyFuncs = results.Select(result => new Func<int, bool>(_ => result));
+            var orAllPropertiesFsFunc = FSharpFunc<int, Property>.FromConverter(
+                n => OrAllProperties(n, propertyFuncs));
+            var ex = Assert.Throws<Exception>(() => Check.QuickThrowOnFailure(orAllPropertiesFsFunc));
+            Assert.That(ex.Message, Is.StringStarting("Falsifiable"));
+        }
+
+        private static Property AndProperties(int n, Func<int, bool> leftPropertyFunc, Func<int, bool> rightPropertyFunc)
+        {
+            var p1 = PropExtensions.Label(leftPropertyFunc(n), "Left property");
+            var p2 = PropExtensions.Label(rightPropertyFunc(n), "Right property");
+            return PropExtensions.And(p1, p2);
+        }
+
+        private static Property OrProperties(int n, Func<int, bool> leftPropertyFunc, Func<int, bool> rightPropertyFunc)
+        {
+            var p1 = PropExtensions.Label(leftPropertyFunc(n), "Left property");
+            var p2 = PropExtensions.Label(rightPropertyFunc(n), "Right property");
+            return PropExtensions.Or(p1, p2);
+        }
+
+        private static Property AndAllProperties(int n, IEnumerable<Func<int, bool>> propertyFuncs)
+        {
+            var properties = propertyFuncs.Select((pf, index) => PropExtensions.Label(pf(n), string.Format("Property[{0}]", index)));
+            return PropExtensions.AndAll(properties.ToArray());
+        }
+
+        private static Property OrAllProperties(int n, IEnumerable<Func<int, bool>> propertyFuncs)
+        {
+            var properties = propertyFuncs.Select((pf, index) => PropExtensions.Label(pf(n), string.Format("Property[{0}]", index)));
+            return PropExtensions.OrAll(properties.ToArray());
         }
     }
 }
